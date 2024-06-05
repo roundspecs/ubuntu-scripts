554c444e5b83adeada6d75aca89072ddfdc6b325
Zarif <2502.zarif@gmail.com>
2024-06-05 06:46:02 +0600

    chore: Update post installer script with additional software installations and system updates
    - Added snap packages for Code, Brave, Slack, Discord, and Inkscape
    - Installed APT packages gnome-shell-extension-manager, neofetch, gnome-tweaks, git, and vim
    - Configured global Git user email and name
    - Cleaned up system with autoremove and autoclean
    - Displayed completion message for post installation

--- post_installer.sh
+++ post_installer.sh
@@ -6,15 +6,22 @@ sudo snap install --classic code
 sudo snap install brave
 sudo snap install --classic slack
 sudo snap install discord
-sudo snap install inkscape
 
 # APT packages
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install gnome-shell-extension-manager neofetch gnome-tweaks git vim -y
+sudo apt install fonts-firacode -y
+sudo apt install qbittorrent -y
+
+# OBS
+sudo apt install v4l2loopback-dkms
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install obs-studio
 
 # Git configuration
+git config --global user.name "Zarif Muhtasim"
 git config --global user.email "2502.zarif@gmail.com"
-git config --global user.name "Zarif"
 
 # Cleanup
 sudo apt autoremove -y
